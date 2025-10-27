c916bc39e5811f72bc14aa08f250435e5174cd03
umesh bharude <umeshbharude253@gmail.com>
2025-10-27 18:24:55 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("welcome to the devops world!");
-        System.out.println("Welcome to the jenkins CI/CD world");
+        System.out.println("Welcome to the jenkins CI/CD world!!!!!");
     }
 }
